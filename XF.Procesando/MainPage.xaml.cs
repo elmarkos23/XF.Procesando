ea4b35b0cbbefda5703779d2f657bea9283f99ac
Markos Ayala <elmarkos23@gmail.com>
2019-02-22 17:26:27 -0500

    Initial commit

---
+++ XF.Procesando/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF.Procesando
+{
+    public partial class MainPage : ContentPage
+    {
+        Procesando objProcesando = new Procesando();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await objProcesando.CargandoPagina(true);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                await objProcesando.CargandoPagina(false);
+            }
+        }
+    }
+}
---
+++ XF.Procesando/Procesando.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Rg.Plugins.Popup.Extensions;
+using Rg.Plugins.Popup.Pages;
+using System.Threading.Tasks;
+using Xamarin.Forms.Xaml;
+
+namespace XF.Procesando
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Procesando : PopupPage
+    {
+		public Procesando ()
+		{
+			InitializeComponent ();
+		}
+        public async Task CargandoPagina(bool activo)
+        {
+            var LoadingPage = new Procesando();
+            if (activo)
+            {
+                await Navigation.PushPopupAsync(LoadingPage);
+
+            }
+            else
+            {
+                await Navigation.PopPopupAsync();
+            }
+
+        }
+    }
+}
